b8dca70ca9a84f3e19fe7e41021aa1babed36b4d
bakunobu <bakunobu@gmail.com>
2021-07-16 20:40:49 +0300

    'Interface basics added'

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # time_bump
+A convinient timetracker project:
+Goals:
+- must be minimalistic;
+- easy daythrought worktime tracking;
+- simple GUI using tk;
+- task management;
+- save data to a DB;
+- interaction with an API (create, add, manage).
+
+
+Design:
+- single window
+- two fields;
+- one picture;
+- three buttons.
+
+
+Functions:
+- start countdown;
+- pause countdown;
+- bump countdown to the file (date added automatically)
+
+
+=====================
+Development history.
+*** 2021/07/16 ***
+Basic GUI structure:
+- main image (should be replaced by a more appropriate one);
+- buttons (tune sizes);
+
+  
--- main.py
+++ main.py
@@ -1,5 +1,37 @@
+import tkinter as tk
+from PIL import Image,  ImageTk
+
 #
 
 
 # create an instance
-pass
+window = tk.Tk()
+window.title('Time BUMP!')
+window.config(padx=20,
+              pady=20)
+
+
+canvas = tk.Canvas(width=640,
+                   height=480,
+                   highlightthickness=0)
+my_image = Image.open('images/bomb.png')
+logo = ImageTk.PhotoImage(my_image.resize((500, 500))) 
+canvas.create_image(320, 240, image=logo)
+canvas.grid(column=1,
+            row=1)
+
+
+start_button = tk.Button(text='Start')
+start_button.grid(column=0,
+                  row=3)
+
+stop_button = tk.Button(text='Pause')
+stop_button.grid(column=2,
+                  row=3)
+
+
+bump_button = tk.Button(text='BUMP!')
+bump_button.grid(column=1,
+                 row=4)
+
+window.mainloop()
