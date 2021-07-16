9b996f23be1ebdfd5a217520ee2dc4dbf607a3bb
bakunobu <bakunobu@gmail.com>
2021-07-16 08:59:37 +0300

    'some basic sctructure and goals'

---
+++ interface.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+
+
+
+class Interface():
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.color('green')
+        self.canvas = tk.Canvas()
+        self.window.mainloop()
+        
+
+# basic class options
+
+
+# functions
+# timer
+# start timer
+# stop timer
+# data manipulation
+
+
+# canvas
+
+
+# buttons
+
+
---
+++ main.py
@@ -0,0 +1,5 @@
+#
+
+
+# create an instance
+pass
